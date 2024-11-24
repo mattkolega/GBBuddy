d402bbcfdfbc52ce2494478f9aff94e6c02c7fc5
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-11-24 20:56:44 +1100

    Add interrupt flag and fix sp and pc size

--- src/cpu.zig
+++ src/cpu.zig
@@ -9,12 +9,14 @@ pub const CPU = packed struct {
     E: u8,
     H: u8,
     L: u8,
-    SP: u8,  // Stack pointer
-    PC: u8,  // Program counter
+    SP: u16,  // Stack pointer
+    PC: u16,  // Program counter
 
     // Flags
     zero: u1,
     subtract: u1,
     halfCarry: u1,
     carry: u1,
+
+    ime: u1,  // Interrupt master enable flag
 };
