252b51ad2ff3a3708c1b8c878256020fa2ef107c
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-12-07 19:10:42 +1100

    Fix implementation errors

--- src/gameboy.zig
+++ src/gameboy.zig
@@ -1,13 +1,18 @@
 //! Implementation of Game Boy device
 
-const CPU = @import("cpu.zig").CPU;
+const cpu = @import("cpu.zig");
 const MMU = @import("mmu.zig").MMU;
 
 pub const GameBoy = struct {
-    cpu: CPU,
+    cpu: cpu.CPU,
     mmu: MMU,
 
-    fn init(self: *GameBoy) void {
+    pub fn init(self: *GameBoy) void {
+        self.cpu.init(self);
         self.mmu.init(self);
+    }
+
+    pub fn run(self: *GameBoy) void {
+        _ = cpu.fetchAndExecute(&self.cpu);
     }
 };
--- src/mmu.zig
+++ src/mmu.zig
@@ -4,33 +4,26 @@ const GameBoy = @import("gameboy.zig").GameBoy;
 const log = @import("logger.zig");
 
 pub const MMU = struct {
-    ram: u8[1024 * 64],  // Temporary 64KB will gradually be reduced to actual RAM size as proper memory mapping is introduced
+    ram: [1024 * 64]u8,  // Temporary 64KB will gradually be reduced to actual RAM size as proper memory mapping is introduced
 
     gb: *GameBoy,
 
-    fn init(self: *MMU, gb: *GameBoy) void {
+    pub fn init(self: *MMU, gb: *GameBoy) void {
         self.gb = gb;
     }
 
-    fn memoryRead(self: *MMU, address: u16) u8 {
+    pub fn memoryRead(self: *MMU, address: u16) u8 {
         switch (address) {
             0x0000...0xFFFF => {
                 return self.ram[address];
             },
-            else => {
-                log.err("Illegal memory read. Invalid memory address: ${X:0>4}", .{ address });
-                return 0x0;
-            },
         }
     }
 
-    fn memoryWrite(self: *MMU, address: u16, value: u8) void {
+    pub fn memoryWrite(self: *MMU, address: u16, value: u8) void {
         switch (address) {
             0x0000...0xFFFF => {
                 self.ram[address] = value;
-            },
-            else => {
-                log.err("Illegal memory write. Invalid memory address: ${X:0>4}", .{ address });
             },
         }
     }
