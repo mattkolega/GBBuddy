0127f6a42b5ef852414678cc19b284f52b4866cb
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-12-20 21:44:01 +1100

    Improve comment formatting

--- src/logger.zig
+++ src/logger.zig
@@ -15,27 +15,23 @@ fn log(comptime prefix: []const u8, comptime fmt: []const u8, args: anytype) voi
     stderr.print(prefix ++ fmt ++ "\n", args) catch return; // Silently return from function if error occurs
 }
 
-/// Logs a message which is useful for debugging and isn't
-/// relevant to the user
+/// Logs a message which is useful for debugging and isn't relevant to the user
 pub fn debug(comptime fmt: []const u8, args: anytype) void {
     log("[DEBUG]: ", fmt, args);
 }
 
-/// Logs a general message which informs the user about the
-/// state of the program
+/// Logs a general message which informs the user about the state of the program
 pub fn info(comptime fmt: []const u8, args: anytype) void {
     log("[INFO]: ", fmt, args);
 }
 
-/// Logs a warning message which indicates that something
-/// may have gone wrong but isn't necessarily an error
+/// Logs a warning message which indicates that something may have gone wrong but isn't necessarily an error
 pub fn warn(comptime fmt: []const u8, args: anytype) void {
     log("[WARN]: ", fmt, args);
 }
 
-/// Logs an error message which indicates that something
-/// has gone wrong during program execution. May or may not
-/// lead to program exit.
+/// Logs an error message which indicates that something has gone wrong during program execution.
+/// May or may not lead to program exit
 pub fn err(comptime fmt: []const u8, args: anytype) void {
     log("[ERROR]: ", fmt, args);
 }
