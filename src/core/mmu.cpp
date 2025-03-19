8346bcb16701357ae3061f69f705120693c95f86
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2025-03-19 19:45:49 +1100

    Add MockMMU to be used for testing

--- src/core/mmu.cpp
+++ src/core/mmu.cpp
@@ -5,12 +5,12 @@
 #include "cartridge.h"
 #include "gameboy.h"
 
-MMU::MMU(GameBoy *gb)
+RealMMU::RealMMU(GameBoy *gb)
     : m_gb(gb)
 {
 }
 
-uint8_t MMU::memoryRead(uint16_t address) {
+uint8_t RealMMU::memoryRead(uint16_t address) {
     if (address >= 0x0000 && address <= 0x7FFF) {
         return m_gb->cartridge.romRead(address);
     } else if (address >= 0x8000 && address <= 0xDFFF) {
@@ -27,7 +27,7 @@ uint8_t MMU::memoryRead(uint16_t address) {
     }
 }
 
-void MMU::memoryWrite(uint16_t address, uint8_t value) {
+void RealMMU::memoryWrite(uint16_t address, uint8_t value) {
     if (address >= 0x0000 && address <= 0x7FFF) {
         m_gb->cartridge.romWrite(address, value);
     } else if (address >= 0x8000 && address <= 0xDFFF) {
@@ -35,6 +35,23 @@ void MMU::memoryWrite(uint16_t address, uint8_t value) {
     } else if (address >= 0xE000 && address <= 0xFDFF) {
         m_memory[address - 0x2000] = value;
     } else if (address >= 0xFE00 && address <= 0xFFFF) {
+        m_memory[address] = value;
+    } else {
+        Logger::err("{} {:X}", "Invalid address for memory write: ", address);
+    }
+}
+
+uint8_t MockMMU::memoryRead(uint16_t address) {
+    if (address >= 0x0000 && address <= 0xFFFF) {
+        return m_memory[address];
+    } else {
+        Logger::err("{} {:X}", "Invalid address for memory read: ", address);
+        return 0x00;
+    }
+}
+
+void MockMMU::memoryWrite(uint16_t address, uint8_t value) {
+    if (address >= 0x0000 && address <= 0xFFFF) {
         m_memory[address] = value;
     } else {
         Logger::err("{} {:X}", "Invalid address for memory write: ", address);
--- src/core/mmu.h
+++ src/core/mmu.h
@@ -4,16 +4,36 @@
 
 class GameBoy;
 
+// Abstract implementation of the MMU
 class MMU {
+public:
+    virtual ~MMU() = default;
+
+    virtual uint8_t memoryRead(uint16_t address) = 0;
+    virtual void memoryWrite(uint16_t address, uint8_t value) = 0;
+};
+
+// Proper MMU implementation to be used in the emulator
+class RealMMU : public MMU {
 private:
     std::array<uint8_t, 1024 * 64> m_memory {};  // Temporary 64KB will gradually be reduced to actual RAM size as proper memory mapping is introduced
 
     GameBoy *m_gb { nullptr };
 
 public:
-    MMU() = delete;
-    MMU(GameBoy *gb);
+    RealMMU() = delete;
+    RealMMU(GameBoy *gb);
 
+    uint8_t memoryRead(uint16_t address);
+    void memoryWrite(uint16_t address, uint8_t value);
+};
+
+// Mock MMU implementation ONLY to be used for CPU testing
+class MockMMU : public MMU {
+private:
+    std::array<uint8_t, 1024 * 64> m_memory {};  // Flat 64kb of memory. No registers or mapping
+
+public:
     uint8_t memoryRead(uint16_t address);
     void memoryWrite(uint16_t address, uint8_t value);
 };
