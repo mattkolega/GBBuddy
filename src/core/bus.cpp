b00c1fad650c9060a72e7750e087fbd33c6fa292
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2026-07-05 14:59:39 +1000

    Clean up read and write functions

--- src/core/bus.cpp
+++ src/core/bus.cpp
@@ -5,34 +5,23 @@
 #include "cartridge.h"
 
 u8 RealBus::read(u16 address) {
-    if (address <= 0x7FFF) {
-        return m_cartridge.romRead(address);
-    } else if (address <= 0xDFFF) {
-        return m_memory[address];
-    } else if (address <= 0xFDFF) {
-        return m_memory[address - 0x2000];
-    } else if (address <= 0xFF44) {
-        return m_memory[address];
-    } else if (address <= 0xFFFF) {
-        return m_memory[address];
-    } else {
-        log::warn("Attempted read from unmapped memory at address: {:X}", address);
-        return 0;
-    }
+    if (address <= 0x7FFF) return m_cartridge.romRead(address);
+    if (address <= 0xDFFF) return m_memory[address];
+    if (address <= 0xFDFF) return m_memory[address - 0x2000];
+    if (address <= 0xFF44) return m_memory[address];
+    if (address <= 0xFFFF) return m_memory[address];
+
+    log::warn("Attempted read from unmapped memory at address: {:X}", address);
+    return 0;
 }
 
 void RealBus::write(u16 address, u8 value) {
-    if (address <= 0x7FFF) {
-       m_cartridge.romWrite(address, value);
-    } else if (address <= 0xDFFF) {
-        m_memory[address] = value;
-    } else if (address <= 0xFDFF) {
-        m_memory[address - 0x2000] = value;
-    } else if (address <= 0xFFFF) {
-        m_memory[address] = value;
-    } else {
-        log::warn("Illegal write to unmapped memory at address: {:X}", address);
-    }
+    if (address <= 0x7FFF) { m_cartridge.romWrite(address, value); return; }
+    if (address <= 0xDFFF) { m_memory[address] = value; return; }
+    if (address <= 0xFDFF) { m_memory[address - 0x2000] = value; return; }
+    if (address <= 0xFFFF) { m_memory[address] = value;  return; }
+
+    log::warn("Illegal write to unmapped memory at address: {:X}", address);
 }
 
 u8 MockBus::read(u16 address) {
